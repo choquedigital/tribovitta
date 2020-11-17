6086891728b8d042a94944ef99559ed9e0129eed
Ayres Roux <ayres@labbo.com.br>
2020-11-17 19:40:31 -0300

    Primeiro commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<script src="<?php echo get_bloginfo('template_url') ?>/js/jquery-3.5.1.min.js"></script>
+<script src="<?php echo get_bloginfo('template_url') ?>/js/scripts.js?v=<?php echo time() ?>"></script>
+<?php wp_footer() ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php wp_title() ?></title>
+
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_url') ?>/style.css?v=<?php echo time() ?>">
+
+    <?php wp_head() ?>
+</head>
+<body>
+    
